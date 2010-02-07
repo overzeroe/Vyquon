101209e75b0c1e7f141bd3eac906e2f4e5f444f9
Pie Squared <overzeroe@gmail.com>
2010-02-07 18:36:23 -0500

    Added car, cdr, first, rest

--- external/List.c
+++ external/List.c
@@ -2,11 +2,15 @@
 
 VyObj MakeCons(VyObj*, int);
 VyObj MakeList(VyObj*, int);
+VyObj GetCar(VyObj*, int);
+VyObj GetCdr(VyObj*, int);
 
 /* Initialize the IO functions and library */
 void LoadList(){
     NewFunction("cons", "(x y)", &MakeCons);
     NewFunction("list", "(... all)", &MakeList);
+    NewFunction("car",  "(lst)", &GetCar);
+    NewFunction("cdr",  "(lst)", &GetCdr);
 }
 
 VyObj MakeCons(VyObj* values, int num_args){
@@ -18,4 +22,10 @@ VyObj MakeList(VyObj* values, int num_args){
     for(i = num_args - 1; i >= 0; i--)
         lst = Cons(values[i], lst);
     return lst;
+}
+VyObj GetCar(VyObj* values, int num_args){
+    return Car(values[0]);
+}
+VyObj GetCdr(VyObj* values, int num_args){
+    return Cdr(values[0]);
 }
