c85023c91b79bdea063c0aef317e0d6a03712152
Pie Squared <overzeroe@gmail.com>
2010-02-07 18:34:00 -0500

    Added 'list function

--- external/List.c
+++ external/List.c
@@ -1,12 +1,21 @@
 #include "Vyquon.h"
 
 VyObj MakeCons(VyObj*, int);
+VyObj MakeList(VyObj*, int);
 
 /* Initialize the IO functions and library */
 void LoadList(){
     NewFunction("cons", "(x y)", &MakeCons);
+    NewFunction("list", "(... all)", &MakeList);
 }
 
 VyObj MakeCons(VyObj* values, int num_args){
     return Cons(values[0], values[1]);
+}
+VyObj MakeList(VyObj* values, int num_args){
+    VyObj lst = Nil();
+    int i;
+    for(i = num_args - 1; i >= 0; i--)
+        lst = Cons(values[i], lst);
+    return lst;
 }
